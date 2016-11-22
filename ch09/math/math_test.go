c8e6bf3583b519a5e2eeecb53b60a3d23d5d89fa
ka1em <wangkaiminchn@gmail.com>
2016-11-22 17:17:24 +0800

    introducing-go

--- ch09/math/math_test.go
+++ ch09/math/math_test.go
@@ -4,9 +4,29 @@ import (
 	"testing"
 )
 
+//func TestAverage(t *testing.T) {
+//	v := Average([]float64{1, 1, 2, 2})
+//	if v != 1.5 {
+//		t.Error("expected 1.5, got", v)
+//	}
+//}
+
+type testpair struct {
+	values  []float64
+	average float64
+}
+
+var tests = []testpair{
+	{[]float64{1, 2}, 1.5},
+	{[]float64{1, 1, 1, 1, 1, 1}, 1},
+	{[]float64{-1, 1}, 0},
+}
+
 func TestAverage(t *testing.T) {
-	v := Average([]float64{1, 1, 2, 2})
-	if v != 1.5 {
-		t.Error("expected 1.5, got", v)
+	for _, pair := range tests {
+		v := Average(pair.values)
+		if v != pair.average {
+			t.Error("For", pair.values, "ecpected", pair.average, "got", v)
+		}
 	}
 }
